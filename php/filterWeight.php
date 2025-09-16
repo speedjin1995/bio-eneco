220f84592714af76101c692d0c058656d355315c
JihB_0105 <ngjihbin1234@gmail.com>
2025-09-16 20:57:41 +0800

    restrict normal to only show today date and only their own created weight record

--- php/filterWeight.php
+++ php/filterWeight.php
@@ -3,6 +3,9 @@
 require_once 'db_connect.php';
 session_start();
 
+## User Role
+$role = $_SESSION["roles"];
+
 ## Read value
 $draw = $_POST['draw'];
 $row = $_POST['start'];
@@ -13,7 +16,11 @@ $columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
 $searchValue = mysqli_real_escape_string($db,$_POST['search']['value']); // Search value
 
 ## Search 
-$searchQuery = " ";
+if ($role == 'NORMAL') {
+  $searchQuery = " and created_by = '".$_SESSION["username"]."'";
+}else{
+  $searchQuery = " ";
+}
 
 if($_POST['fromDate'] != null && $_POST['fromDate'] != ''){
   $dateTime = DateTime::createFromFormat('d-m-Y', $_POST['fromDate']);
